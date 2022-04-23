9ff81d7978d2bc930e37e55e5b4c45992429871f
Christian <christianmmartinez0823@gmail.com>
2022-04-23 17:38:08 -0600

    added css

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,6 @@
-let date = new Date();
-console.log(date);
 
-let year = date.getFullYear();
-console.log(year);
 
-// const myarray = ["Lesson 02:",
-// "Lesson 03:",
-// "Lesson 04:",
-// "Lesson 05:",
-// "Lesson 06:",
-// "Lesson 07:",
-// "Lesson 08:",
-// "Lesson 09:",
-// "Lesson 10:",
-// "Lesson 11:"
-// ];
 
-// function makelist(item) {
-//     document.querySelector('div').innerHTML += '<li> + ${item} + </li>'
-// }
+document.getElementById("update").innerHTML = "Last Updated: " + document.lastModified;
 
-// myarray.forEach(makelist);
+
